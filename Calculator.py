f7bdb5420e6c608dabd8feea845eb774cc140a1a
Rayyan Rasheed <rayyankiddy@gmail.com>
2020-07-12 10:03:31 +0530

    Button Frames Added

--- Calculator.py
+++ Calculator.py
@@ -7,7 +7,19 @@ root.geometry("300x500+450+100")
 root.resizable(0,0)
 root.title("Calculator")
 
+btnrow1 = Frame(root, bg ="#000000")
+btnrow1.pack(expand = True, fill = "both")
 
 
+btnrow2 = Frame(root)
+btnrow2.pack(expand = True, fill = "both")
+
+
+btnrow3 = Frame(root)
+btnrow3.pack(expand = True, fill = "both")
+
+
+btnrow4 = Frame(root)
+btnrow4.pack(expand = True, fill = "both")
 
 root.mainloop()
